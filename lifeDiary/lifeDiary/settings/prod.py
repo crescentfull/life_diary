9152206887d4b9ae0c1ca1feb9ecd353bf04ab40
Yeongrok Song <song7868@gmail.com>
2025-07-25 21:27:01 +0900

    배포 준비#2

--- lifeDiary/lifeDiary/settings/prod.py
+++ lifeDiary/lifeDiary/settings/prod.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["lifediary.onrender.com"]
 
 
 # Application definition
