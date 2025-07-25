962374283f629452791ede5f7536898501a16ff9
Yeongrok Song <song7868@gmail.com>
2025-07-25 21:52:32 +0900

    배포 준비#4 > DB 변경

--- lifeDiary/lifeDiary/settings/prod.py
+++ lifeDiary/lifeDiary/settings/prod.py
@@ -81,14 +81,11 @@ WSGI_APPLICATION = "lifeDiary.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
