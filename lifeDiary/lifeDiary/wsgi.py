f1a644677684e1e4304057fed76abc3d9fc084aa
Yeongrok Song <song7868@gmail.com>
2025-07-25 21:23:20 +0900

    배포 준비

--- lifeDiary/lifeDiary/wsgi.py
+++ lifeDiary/lifeDiary/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lifeDiary.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lifeDiary.lifeDiary.settings.prod")
 
 application = get_wsgi_application()
